3acb352c32bb0ddede9b880dfeb3319520573ffd
scizeron <stephane.cizeron@gmail.com>
2015-04-09 16:58:40 +0200

    UI application wich performs ec2 start|stop commands

--- src/main/java/com/stfciz/aws/ec2/data/EC2InstancesManager.java
+++ src/main/java/com/stfciz/aws/ec2/data/EC2InstancesManager.java
@@ -26,6 +26,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -35,154 +36,175 @@ import java.util.ResourceBundle;
  */
 public class EC2InstancesManager {
 
-    public final static String CREDENTIALS_FILE = System.getProperty("user.home") +  File.separator + ".ec2" + File.separator + "AwsCredentials.properties";
+  public final static String CREDENTIALS_FILE = System.getProperty("user.home") + File.separator + ".ec2" + File.separator + "AwsCredentials.properties";
 
-    private AWSCredentials credentials = null;
+  private AWSCredentials credentials = null;
 
-    private AmazonEC2Client amazonEC2Client = null;
+  private AmazonEC2Client amazonEC2Client = null;
 
-    private List<String> regions;
+  private List<String> regions;
 
-    /**
-     *
-     */
-    public EC2InstancesManager() throws IOException {
-        loadCredentials();
-        final Enumeration<String> keys = ResourceBundle.getBundle("regions").getKeys();
-        List<String> regionValues = new ArrayList<>();
-        while (keys.hasMoreElements()) {
-            regionValues.add(keys.nextElement());
-        }
-        this.regions = Collections.unmodifiableList(regionValues);
+  private String [] tagIncludes;
+
+  /**
+   *
+   */
+  public EC2InstancesManager() throws IOException {
+    loadCredentials();
+    final Enumeration<String> keys = ResourceBundle.getBundle("regions").getKeys();
+    List<String> regionValues = new ArrayList<>();
+    while (keys.hasMoreElements()) {
+      regionValues.add(keys.nextElement());
+    }
+    this.regions = Collections.unmodifiableList(regionValues);
+    try {
+      final ResourceBundle filter = ResourceBundle.getBundle("instances-filter");
+      if (filter != null) {
+        this.tagIncludes = filter.getString("tag.includes").split(",");
+      }
+    } catch(MissingResourceException missingResourceException) {
+      /** NOOP **/
+    }
+  }
+
+  /**
+   *
+   * @param instanceId
+   */
+  public void startInstance(String[] instanceIds) {
+    StartInstancesRequest request = new StartInstancesRequest();
+    request.setInstanceIds(Arrays.asList(instanceIds));
+    this.amazonEC2Client.startInstances(request);
+  }
+
+  /**
+   *
+   * @param instanceId
+   */
+  public void stopInstance(String[] instanceIds) {
+    StopInstancesRequest request = new StopInstancesRequest();
+    request.setInstanceIds(Arrays.asList(instanceIds));
+    this.amazonEC2Client.stopInstances(request);
+  }
+
+  /**
+   *
+   * @return
+   */
+  public boolean hasCredentials() {
+    return this.credentials != null;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public String getAWSAccessKeyId() {
+    return this.credentials != null ? this.credentials.getAWSAccessKeyId() : null;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public String getAWSSecretKey() {
+    return this.credentials != null ? this.credentials.getAWSSecretKey() : null;
+  }
+
+  /**
+   *
+   * @return
+   */
+  public List<String> getRegions() {
+    return this.regions;
+  }
+
+  /**
+   *
+   * @param accessKeyId
+   * @param secretKey
+   * @throws IOException
+   */
+  public void save(String accessKeyId, String secretKey) throws IOException {
+    Properties properties = new Properties();
+    properties.put("accessKey", accessKeyId);
+    properties.put("secretKey", secretKey);
+
+    File parent = new File(CREDENTIALS_FILE).getParentFile();
+
+    if (!parent.exists()) {
+      parent.mkdirs();
     }
 
-    /**
-     * 
-     * @param instanceId 
-     */
-    public void startInstance(String[] instanceIds) {
-        StartInstancesRequest request = new StartInstancesRequest();
-        request.setInstanceIds(Arrays.asList(instanceIds));
-        this.amazonEC2Client.startInstances(request);
-    }
-    
-        /**
-     * 
-     * @param instanceId 
-     */
-    public void stopInstance(String[] instanceIds) {
-        StopInstancesRequest request = new StopInstancesRequest();
-        request.setInstanceIds(Arrays.asList(instanceIds));
-        this.amazonEC2Client.stopInstances(request);
-    }
-    
-    /**
-     *
-     * @return
-     */
-    public boolean hasCredentials() {
-        return this.credentials != null;
-    }
+    properties.store(new FileOutputStream(new File(CREDENTIALS_FILE)), null);
 
-    /**
-     *
-     * @return
-     */
-    public String getAWSAccessKeyId() {
-        return this.credentials != null ? this.credentials.getAWSAccessKeyId() : null;
+    loadCredentials();
+  }
+
+  /**
+   *
+   * @param region
+   * @return
+   */
+  public List<EC2Instance> getEC2Instances(String region) {
+    if (this.credentials == null) {
+      return null;
     }
-
-    /**
-     *
-     * @return
-     */
-    public String getAWSSecretKey() {
-        return this.credentials != null ? this.credentials.getAWSSecretKey() : null;
+    List<EC2Instance> ec2Instances;
+    ec2Instances = new ArrayList<>();
+    if (region != null) {
+      this.amazonEC2Client.setRegion(Region.getRegion(Regions.fromName(region)));
     }
+    final DescribeInstancesResult describeInstances = this.amazonEC2Client.describeInstances();
+    final List<Reservation> reservations = describeInstances.getReservations();
+    for (Reservation reservation : reservations) {
+      final List<Instance> instances = reservation.getInstances();
+      for (Instance instance : instances) {
+        EC2Instance ec2Instance = new EC2Instance();
+        ec2Instance.setId(instance.getInstanceId());
+        List<Tag> tags = instance.getTags();
 
-    /**
-     *
-     * @return
-     */
-    public List<String> getRegions() {
-        return this.regions;
-    }
-
-    /**
-     *
-     * @param accessKeyId
-     * @param secretKey
-     * @throws IOException
-     */
-    public void save(String accessKeyId, String secretKey) throws IOException {
-        Properties properties = new Properties();
-        properties.put("accessKey", accessKeyId);
-        properties.put("secretKey", secretKey);
-        
-        File parent = new File(CREDENTIALS_FILE).getParentFile();
-        
-        if (!parent.exists()) {
-          parent.mkdirs();
-        }
-        
-        properties.store(new FileOutputStream(new File(CREDENTIALS_FILE)), null);
-
-        loadCredentials();
-    }
-
-    /**
-     *
-     * @param region
-     * @return
-     */
-    public List<EC2Instance> getEC2Instances(String region) {
-        if (this.credentials == null) {
-            return null;
-        }
-        List<EC2Instance> ec2Instances;
-        ec2Instances = new ArrayList<>();
-        if (region != null) {
-            this.amazonEC2Client.setRegion(Region.getRegion(Regions.fromName(region)));
-        }
-        final DescribeInstancesResult describeInstances = this.amazonEC2Client.describeInstances();
-        final List<Reservation> reservations = describeInstances.getReservations();
-        for (Reservation reservation : reservations) {
-            final List<Instance> instances = reservation.getInstances();
-            for (Instance instance : instances) {
-                EC2Instance ec2Instance = new EC2Instance();
-                ec2Instance.setId(instance.getInstanceId());
-                List<Tag> tags = instance.getTags();
-
-                if (tags != null && !tags.isEmpty()) {
-                    StringBuilder name = new StringBuilder();
-                    for (Tag tag : tags) {
-                        if (name.length() > 0) {
-                            name.append(", ");
-                        }
-                        name.append(tag.getValue());
-                    }
-                    ec2Instance.setName(name.toString());
-                }
-                ec2Instance.setPublicDnsName(instance.getPublicDnsName());
-                final InstanceState state = instance.getState();
-                ec2Instance.setStatus(state.getName());
-                ec2Instance.setStatusCode(state.getCode());
-                ec2Instances.add(ec2Instance);
+        if (tags != null && !tags.isEmpty()) {
+          StringBuilder name = new StringBuilder();
+          for (Tag tag : tags) {
+            if (name.length() > 0) {
+              name.append(", ");
             }
+            name.append(tag.getValue());
+          }
+          ec2Instance.setName(name.toString());
         }
-
-        return ec2Instances;
+        
+        final InstanceState state = instance.getState();
+        ec2Instance.setStatus(state.getName());
+        ec2Instance.setStatusCode(state.getCode());
+ 
+        if (this.tagIncludes != null && ec2Instance.getName() != null) {
+          for (String tagInclude : this.tagIncludes) {
+            if (ec2Instance.getName().contains(tagInclude)) {
+              ec2Instance.setPublicDnsName(instance.getPublicDnsName());
+              ec2Instances.add(ec2Instance);
+              break;
+            }
+          }
+        } else {
+          ec2Instances.add(ec2Instance);
+        }
+      }
     }
 
-    /**
-     *
-     * @throws IOException
-     */
-    private void loadCredentials() throws IOException {
-        File propertiesCredentialsFile = new File(CREDENTIALS_FILE);
-        if (propertiesCredentialsFile.exists()) {
-            this.credentials = new PropertiesCredentials(propertiesCredentialsFile);
-            this.amazonEC2Client = new AmazonEC2Client(this.credentials);
-        }
+    return ec2Instances;
+  }
+
+  /**
+   *
+   * @throws IOException
+   */
+  private void loadCredentials() throws IOException {
+    File propertiesCredentialsFile = new File(CREDENTIALS_FILE);
+    if (propertiesCredentialsFile.exists()) {
+      this.credentials = new PropertiesCredentials(propertiesCredentialsFile);
+      this.amazonEC2Client = new AmazonEC2Client(this.credentials);
     }
+  }
 }
